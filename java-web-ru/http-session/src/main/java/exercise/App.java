2e9ea3301eac67911e77b99156b871bd693d1de4
@hexlet/cli <support@hexlet.io>
2024-09-23 23:12:08 +0300

    submit java-web-ru/http-session

---
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -0,0 +1,44 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public final class App {
+
+    private static final List<Map<String, String>> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/users", ctx -> {
+            var neededUsers = new ArrayList<>();
+            int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+            int per = ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
+            var i = ((page - 1) * per) - 1;
+            if (i < 0) {
+                i = 0;
+            }
+            var users = Data.getUsers();
+            for (; i < page * per; i++) {
+                neededUsers.add(users.get(i));
+            }
+            ctx.json(neededUsers);
+        });
+        // END
+
+        return app;
+
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
