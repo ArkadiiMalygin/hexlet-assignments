16b80cd81c76b3e3db5a46413f54ffd205273e96
@hexlet/cli <support@hexlet.io>
2024-10-22 02:54:26 +0300

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/Application.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private PersonRepository personRepository;
+}
