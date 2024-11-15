b8818d38f9a87e1b2ef64782673d5d4c95678af2
@hexlet/cli <support@hexlet.io>
2024-11-15 02:18:31 +0200

    submit java-spring-ru/beans

---
+++ java-spring-ru/beans/src/main/java/exercise/Application.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+import exercise.daytime.Daytime;
+import exercise.daytime.Day;
+import exercise.daytime.Night;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+// BEGIN
+
+// END
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @Scope("prototype")
+    @Bean
+    public Daytime getTime() {
+        return (LocalDateTime.now().getHour() >= 6 && LocalDateTime.now().getHour() < 22) ? new Day() : new Night();
+    }
+    // END
+}
